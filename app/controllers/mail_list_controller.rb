2f03a2fd667c204bd145aa8853dac350e5538120
postazure <gordonmaxc@gmail.com>
2014-12-04 14:13:18 -0800

    prevent stack overflow potential in draw

--- app/controllers/mail_list_controller.rb
+++ app/controllers/mail_list_controller.rb
@@ -1,18 +1,35 @@
 class MailListController < ApplicationController
   def draw
     friends = Friend.where(group_id: params[:group_id].to_i)
+
     givers = friends.map {|item| item}
     recievers = friends.shuffle
 
     selection_list = []
-    givers.each do |giver|
-      recievers.each do |reciever|
-        if giver.name != reciever.name && giver.spouse != reciever.name
-          selection_list.push([giver, reciever])
-          recievers.delete(reciever)
-          break
+
+    if friends.count >= 4
+
+      givers.each do |giver|
+        recievers.each do |reciever|
+          if giver.name != reciever.name && giver.spouse != reciever.name
+            selection_list.push([giver, reciever])
+            recievers.delete(reciever)
+            break
+          end
         end
       end
+
+    else
+      givers.each do |giver|
+        recievers.each do |reciever|
+          if giver.name != reciever.name
+            selection_list.push([giver, reciever])
+            recievers.delete(reciever)
+            break
+          end
+        end
+      end
+
     end
 
     if selection_list.length < givers.length
@@ -20,7 +37,5 @@ class MailListController < ApplicationController
     else
       @mailing_list = selection_list
     end
-
-    # @mailing_list = [params[:group_id].to_i]
   end
 end
